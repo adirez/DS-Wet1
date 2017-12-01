9c963aa63eaa512c385f4f6267291de242152335
adirez <adireznik@campus.technion.ac.il>
2017-12-01 12:43:41 +0200

    library finished

--- library1.cpp
+++ library1.cpp
@@ -2,3 +2,160 @@
 // Created by adire on 30-Nov-17.
 //
 
+#include "library1.h"
+#include "Colosseum.h"
+#include "iostream"
+
+using std::cout;
+using std::endl;
+
+void *init() {
+    Colosseum *DS;
+    try {
+        DS = new Colosseum();
+    } catch (...) {
+        return NULL;
+    }
+    return DS;
+}
+
+StatusType AddTrainer(void *DS, int trainerID) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
+    Colosseum *colosseum = (Colosseum *) DS;
+    try {
+        colosseum->addTrainer(trainerID);
+    } catch (MemoryProblem &e) {
+        return ALLOCATION_ERROR;
+    } catch (InvalidParameter &e) {
+        return INVALID_INPUT;
+    } catch (KeyAlreadyExists &e) {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType BuyGladiator(void *DS, int gladiatorID, int trainerID, int level) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
+    Colosseum *colosseum = (Colosseum *) DS;
+    try {
+        colosseum->buyGladiator(gladiatorID, trainerID, level);
+    } catch (MemoryProblem &e) {
+        return ALLOCATION_ERROR;
+    } catch (InvalidParameter &e) {
+        return INVALID_INPUT;
+    } catch (KeyAlreadyExists &e) {
+        return FAILURE;
+    } catch (KeyNotFound &e) {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType FreeGladiator(void *DS, int gladiatorID) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
+    Colosseum *colosseum = (Colosseum *) DS;
+    try {
+        colosseum->freeGladiator(gladiatorID);
+    } catch (InvalidParameter &e) {
+        return INVALID_INPUT;
+    } catch (KeyNotFound &e) {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType LevelUp(void *DS, int gladiatorID, int levelIncrease) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
+    Colosseum *colosseum = (Colosseum *) DS;
+    try {
+        colosseum->levelUp(gladiatorID, levelIncrease);
+    } catch (MemoryProblem &e) {
+        return ALLOCATION_ERROR;
+    } catch (InvalidParameter &e) {
+        return INVALID_INPUT;
+    } catch (KeyNotFound &e) {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType GetTopGladiator(void *DS, int trainerID, int *gladiatorID) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
+    Colosseum *colosseum = (Colosseum *) DS;
+    try {
+        *gladiatorID = colosseum->getTopGladiator(trainerID);
+    } catch (MemoryProblem &e) {
+        return ALLOCATION_ERROR;
+    } catch (InvalidParameter &e) {
+        return INVALID_INPUT;
+    } catch (KeyNotFound &e) {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType GetAllGladiatorsByLevel(void *DS, int trainerID, int **gladiators, int *numOfGladiator){
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
+    Colosseum *colosseum = (Colosseum *) DS;
+    try {
+        colosseum->getAllGladiatorsByLevel(trainerID, numOfGladiator, gladiators);
+    } catch (MemoryProblem &e) {
+        return ALLOCATION_ERROR;
+    } catch (InvalidParameter &e) {
+        return INVALID_INPUT;
+    } catch (KeyNotFound &e) {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType UpgradeGladiator(void *DS, int gladiatorID, int upgradedID) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
+    Colosseum *colosseum = (Colosseum *) DS;
+    try {
+        colosseum->updateGladiator(gladiatorID, upgradedID);
+    } catch (MemoryProblem &e) {
+        return ALLOCATION_ERROR;
+    } catch (InvalidParameter &e) {
+        return INVALID_INPUT;
+    } catch (KeyNotFound &e) {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType UpdateLevels(void *DS, int stimulantCode, int stimulantFactor){
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
+    Colosseum *colosseum = (Colosseum *) DS;
+    try {
+        colosseum->updateLevels(stimulantCode, stimulantFactor);
+    } catch (MemoryProblem &e) {
+        return ALLOCATION_ERROR;
+    } catch (InvalidParameter &e) {
+        return INVALID_INPUT;
+    } catch (KeyNotFound &e) {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+void Quit(void** DS){
+    Colosseum *colosseum = (Colosseum *) DS;
+    delete colosseum;
+}
